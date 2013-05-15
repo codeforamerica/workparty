6a0525b3ec1dceeb01cb37c998a7dda69c1bbf88
Dave Guarino <dave@codeforamerica.org>
2013-05-15 16:08:16 -0700

    First commit on WorkParty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workparty::Application.config.secret_key_base = '06b3a318b5107812bd079436f8e2108e0fdfb2483d16e28327d328470da27faace54688094f601dc6387232d2175dde34f17db3a55dc6dee94b7bf0ca75e1226'

15e75a594fdd28f532d282a1cbac99a6780ee06a
Dave Guarino <dave@codeforamerica.org>
2013-05-16 15:38:17 -0700

    Prohibit users from C(r)UDing unless logged in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /events
   # GET /events.json

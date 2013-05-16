c03b239651dd280f15c7b4b8fae59b8834e9cb64
Dave Guarino <dave@codeforamerica.org>
2013-05-15 18:09:14 -0700

    Add route for events (resource)

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Workparty::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :events
+
   # Example resource route with options:
   #   resources :products do
   #     member do

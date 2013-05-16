305d482c670c8e8169e15a46573a6c067cc6a153
Dave Guarino <dave@codeforamerica.org>
2013-05-16 12:38:08 -0700

    Basic users#new with signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Workparty::Application.routes.draw do
+  get "users/new"
+  match '/signup', to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

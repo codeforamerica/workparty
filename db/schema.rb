d5ff1c5034e47e154da3905806739edd6450780a
Dave Guarino <dave@codeforamerica.org>
2013-05-16 16:17:30 -0700

    Add phone number to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130516224854) do
+ActiveRecord::Schema.define(version: 20130516230735) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20130516224854) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

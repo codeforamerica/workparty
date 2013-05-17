7a81ec552da3115a4d2f5603cd1c5619a8c058e7
Dave Guarino <dave@codeforamerica.org>
2013-05-16 23:30:57 -0700

    Added conditional login to event pageand make using that redirect you back to the same event page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def after_sign_in_path_for(resource)
+    if request.referrer == new_user_session_url || request.referrer == new_user_registration_url
+      root_path
+    else
+      request.referrer
+    end
+  end
+
 end

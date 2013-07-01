87be86d5e5e44a7bb3509de4ab169ab65fc51df9
Dave Guarino <dave@codeforamerica.org>
2013-06-30 17:32:19 -0700

    Set secret token from ENV in production and raise error if not done

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,10 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Workparty::Application.config.secret_key_base = '06b3a318b5107812bd079436f8e2108e0fdfb2483d16e28327d328470da27faace54688094f601dc6387232d2175dde34f17db3a55dc6dee94b7bf0ca75e1226'
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+	raise 'The SECRET_TOKEN environment variable is not set.\n
+	To generate it, run "rake secret", then set it on the production server.\n
+  If you\'re using Heroku, you do this with "heroku config:set SECRET_TOKEN=the_token_you_generated"'
+end
+
+Workparty::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '06b3a318b5107812bd079436f8e2108e0fdfb2483d16e28327d328470da27faace54688094f601dc6387232d2175dde34f17db3a55dc6dee94b7bf0ca75e1226'
